6d5e049e1e5b5ea47a921fdf84776c75fa167718
garethfoote <gareth.foote@gmail.com>
2015-06-19 10:47:13 +0100

    Amend custom post type

--- app/customPostTypes.php
+++ app/customPostTypes.php
@@ -5,17 +5,17 @@
 \add_action( 'init', function()
 {
     $labels = array(
-        'name'                => _x( 'Quizzes', 'Post Type General Name', 'text_domain' ),
-        'singular_name'       => _x( 'Quiz', 'Post Type Singular Name', 'text_domain' ),
-        'menu_name'           => __( 'Quiz', 'text_domain' ),
+        'name'                => _x( 'Polls', 'Post Type General Name', 'text_domain' ),
+        'singular_name'       => _x( 'Poll', 'Post Type Singular Name', 'text_domain' ),
+        'menu_name'           => __( 'Poll', 'text_domain' ),
         'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
-        'all_items'           => __( 'All Quizzes', 'text_domain' ),
-        'view_item'           => __( 'View Quiz', 'text_domain' ),
-        'add_new_item'        => __( 'Add New Quiz', 'text_domain' ),
+        'all_items'           => __( 'All Polls', 'text_domain' ),
+        'view_item'           => __( 'View Poll', 'text_domain' ),
+        'add_new_item'        => __( 'Add New Poll', 'text_domain' ),
         'add_new'             => __( 'Add New', 'text_domain' ),
-        'edit_item'           => __( 'Edit Quiz', 'text_domain' ),
-        'update_item'         => __( 'Update Quiz', 'text_domain' ),
-        'search_items'        => __( 'Search Quizzes', 'text_domain' ),
+        'edit_item'           => __( 'Edit Poll', 'text_domain' ),
+        'update_item'         => __( 'Update Poll', 'text_domain' ),
+        'search_items'        => __( 'Search Polls', 'text_domain' ),
         'not_found'           => __( 'Not found', 'text_domain' ),
         'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
     );
@@ -33,7 +33,7 @@
       'show_in_nav_menus'   => true,
       'show_in_admin_bar'   => true,
       'menu_position'       => 5,
-      'menu_icon'           => 'dashicons-forms',
+      'menu_icon'           => 'dashicons-editor-alignleft',
       'can_export'          => true,
       'has_archive'         => true,
       'exclude_from_search' => false,
@@ -41,6 +41,6 @@
       'capability_type'     => 'page',
     );
 
-  \register_post_type('quiz', $args);
+  \register_post_type('poll', $args);
 }
 ,0);
