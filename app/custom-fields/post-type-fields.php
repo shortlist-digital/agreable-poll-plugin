3a769412ba323dce32435fdff240e4cece2870d2
garethfoote <gareth.foote@gmail.com>
2015-06-19 10:44:49 +0100

    Ammended custom post type definitions and change reference in herbert config.

---
+++ app/custom-fields/post-type-fields.php
@@ -0,0 +1,99 @@
+<?php
+
+if ( ! class_exists( 'ACF' ) ) {
+  add_action( 'admin_notices', function() {
+    echo '<div class="error"><p>ACF5 Pro is not activated. Make sure you activate the plugin in <a href="' . esc_url( admin_url( 'plugins.php' ) ) . '">' . esc_url( admin_url( 'plugins.php' ) ) . '</a></p></div>';
+  } );
+  return;
+}
+
+if( function_exists('register_field_group') ):
+
+register_field_group(array (
+  'key' => 'slm_poll_definition',
+  'title' => 'Poll Definition',
+  'fields' => array (
+    array (
+      'key' => 'slm_poll_definition_question',
+      'label' => 'Question',
+      'name' => 'poll_question',
+      'prefix' => '',
+      'type' => 'text',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => array (
+        'width' => '',
+        'class' => '',
+        'id' => '',
+      ),
+      'default_value' => '',
+      'placeholder' => '',
+      'prepend' => '',
+      'append' => '',
+      'maxlength' => '',
+      'readonly' => 0,
+      'disabled' => 0,
+    ),
+    array (
+      'key' => 'slm_poll_definition_answers',
+      'label' => 'Answers',
+      'name' => 'poll_answers',
+      'prefix' => '',
+      'type' => 'repeater',
+      'instructions' => '',
+      'required' => 0,
+      'conditional_logic' => 0,
+      'wrapper' => array (
+        'width' => '',
+        'class' => '',
+        'id' => '',
+      ),
+      'min' => '',
+      'max' => '',
+      'layout' => 'table',
+      'button_label' => 'Add an answer',
+      'sub_fields' => array (
+        array (
+          'key' => 'slm_poll_definition_answers_answer_text',
+          'label' => 'Answer / Button Text',
+          'name' => 'answer_text',
+          'prefix' => '',
+          'type' => 'text',
+          'instructions' => '',
+          'required' => 0,
+          'conditional_logic' => 0,
+          'wrapper' => array (
+            'width' => '',
+            'class' => '',
+            'id' => '',
+          ),
+          'default_value' => '',
+          'placeholder' => '',
+          'prepend' => '',
+          'append' => '',
+          'maxlength' => '',
+          'readonly' => 0,
+          'disabled' => 0,
+        ),
+      ),
+    ),
+  ),
+  'location' => array (
+      array (
+          array (
+              'param' => 'post_type',
+              'operator' => '==',
+              'value' => 'poll',
+          ),
+      ),
+  ),
+  'menu_order' => 0,
+  'position' => 'normal',
+  'style' => 'seamless',
+  'label_placement' => 'top',
+  'instruction_placement' => 'label',
+  'hide_on_screen' => '',
+));
+
+endif;
