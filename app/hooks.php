c1cf8f0c9dbe272bd0db58ff82e1a8371ca68df8
garethfoote <gareth.foote@gmail.com>
2015-06-22 12:58:58 +0100

    Changing poll placeholder for title

--- app/hooks.php
+++ app/hooks.php
@@ -12,3 +12,21 @@ if(class_exists('SLM_PollPlugin\Hooks\TimberLoaderPaths')){
 if(class_exists('SLM_PollPlugin\Hooks\SavePost')){
   (new SavePost)->init();
 }
+
+/*
+ * Custom placeholder for poll post type.
+ */
+add_filter('enter_title_here','custom_enter_title');
+if( function_exists('custom_enter_title') ){
+
+  function custom_enter_title( $input ) {
+    global $post_type;
+
+    if(is_admin() && 'Enter title here' == $input && $post_type === 'poll'){
+      return __('Enter poll question');
+    }
+
+    return $input;
+  }
+
+}
