54dd62cbb113d5981ee13c81b5a0077a5fd90426
garethfoote <gareth.foote@gmail.com>
2015-06-24 13:16:07 +0100

    Modified for Croissant use.

--- plugin.php
+++ plugin.php
@@ -11,5 +11,14 @@
  * License:           MIT
  */
 
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/vendor/getherbert/framework/bootstrap/autoload.php';
+if(file_exists(__DIR__ . '/../../../../vendor/autoload.php')){
+  require_once __DIR__ . '/../../../../vendor/autoload.php';
+} else {
+  require_once __DIR__ . '/vendor/autoload.php';
+}
+
+if(file_exists(__DIR__ . '/../../../../vendor/getherbert/framework/bootstrap/autoload.php')){
+  require_once __DIR__ . '/../../../../vendor/getherbert/framework/bootstrap/autoload.php';
+} else {
+  require_once __DIR__ . '/vendor/getherbert/framework/bootstrap/autoload.php';
+}
