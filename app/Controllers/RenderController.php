677ff27d8263b30b0d6ac6257d295a445afca99c
garethfoote <gareth.foote@gmail.com>
2015-09-01 17:04:18 +0100

    Make polls embedable

--- app/Controllers/RenderController.php
+++ app/Controllers/RenderController.php
@@ -4,10 +4,62 @@ use AgreablePollPlugin\Helper;
 
 class RenderController {
 
-  public function enqueue(){
+  /*
+   * Output preview. Necessary for UTF8 encoding of quotes, etc.
+   */
+  public function preview($slug){
+
+    echo <<<HTML
+<!doctype html>
+<html lang="">
+  <head>
+      <meta charset="utf-8">
+  </head>
+  <body>
+HTML;
+
+    $this->embed($slug);
+
+    echo <<<HTML
+    </body>
+</html>
+HTML;
+
+  }
+
+  public function embed($slug){
+
+    $post = get_posts("name=$slug&post_type=poll");
+
+    $context = \Timber::get_context();
+    $context['poll'] = new \TimberPost($post[0]);
+
+    $this->inline_css();
+    $views = trailingslashit(Helper::get('views')['AgreablePollPlugin']);
+	  \Timber::render("$views/widgets/poll_plugin/template.twig", $context);
+    $this->inline_js();
+
+  }
+
+  /*
+   * Enqueue scripts rather than render immediately. Used by widget iterator
+   * to ensure that this is output in the footer after the themes app.js, which
+   * will contain dependencies such as Backbone and jQuery.
+   *
+   * Due to using in the embed CMS context we now output Backbone and jquery into
+   * the built JavaScript file so it contains all dependencies. Therefore we
+   * could just output app.js here. Not doing that right now though.
+   */
+  public function enqueue_js(){
 
     // Enqueue scripts.
     wp_enqueue_script( 'agreable_poll_script', Helper::assetUrl('client.js'), array(), '1.0.0', true );
+  }
+
+  /*
+   * Output built CSS into a <style> tag inline.
+   */
+  public function inline_css(){
 
     /*
      * @AgreablePollPlugin is a Twig namespace which Herbert generates from
@@ -25,6 +77,18 @@ class RenderController {
         'plugin_settings_property_secondary_colour'    => get_field($ns.'_plugin_settings_property_secondary_colour', 'option'),
         'plugin_settings_property_font_family'         => get_field($ns.'_plugin_settings_property_font_family', 'option'),
         'plugin_settings_free_text'                    => get_field($ns.'_plugin_settings_free_text_css', 'option'),
+    ])->getBody();
+  }
+
+  /*
+   * Output build JS into a <script> tag inline.
+   */
+  public function inline_js(){
+
+    $js = file_get_contents(Helper::asset('client.js'));
+    echo view('@AgreablePollPlugin/scripts.twig', [
+        'js_path'   => Helper::asset('client.js'),
+        'js'   => $js,
     ])->getBody();
 
   }
--- app/Hooks/SLMPluginEnqueue.php
+++ app/Hooks/SLMPluginEnqueue.php
@@ -5,12 +5,16 @@ use AgreablePollPlugin\Controllers\RenderController;
 class SLMPluginEnqueue {
 
   public function init() {
-    \add_action('agreable_poll_plugin_enqueue', array($this, 'plugin_enqueue'), 10, 0);
+    \add_action('agreable_poll_plugin_enqueue', array($this, 'plugin_assets'), 10, 0);
   }
 
-  public function plugin_enqueue(){
+  public function plugin_assets(){
     $r = new RenderController();
-    $r->enqueue();
+    // JavaScript is enqueued (instead of rendered inline) so that it is called
+    // after the markup is rendered.
+    $r->enqueue_js();
+    // CSS can be rendered inline just before the markup.
+    $r->inline_css();
   }
 
 }
